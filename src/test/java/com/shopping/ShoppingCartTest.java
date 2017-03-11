753aad5d1cceb2d46a0eddd1ee3735e79c16f44d
Daniel Aquino <daniel.aquino@digitalreasoning.com>
2017-03-11 14:05:20 -0600

    Test JUnit setup.

---
+++ src/test/java/com/shopping/ShoppingCartTest.java
@@ -0,0 +1,11 @@
+package com.shopping;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShoppingCartTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, 1);
+    }
+}
